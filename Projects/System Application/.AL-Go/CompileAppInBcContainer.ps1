975cdc729cbadbef306ce1a2f55f4751fd0e48f1
Alexander Holstrup <aholstrup@microsoft.com>
2023-05-11 17:25:32 +0200

    Fix for projectFolder path

--- Projects/System Application/.AL-Go/CompileAppInBcContainer.ps1
+++ Projects/System Application/.AL-Go/CompileAppInBcContainer.ps1
@@ -3,6 +3,6 @@ Param(
 )
 
 $scriptPath = Join-Path $PSScriptRoot "../../../Build/Scripts/CompileAppInBcContainer.ps1" -Resolve
-$projectFolder = Join-Path $PSScriptRoot "../../../System Application"
+$projectFolder = Join-Path $PSScriptRoot "../../System Application"
 
 . $scriptPath -parameters $parameters -currentProjectFolder $projectFolder
