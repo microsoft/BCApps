8ac7621e7dff272ccb0f38cf5c737dbcb65876d7
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-07-12 16:15:28 +0200

    Add Sync Pipeline  (#67)
    This PR sets up an ADO pipeline that can sync changes between GitHub and
    ADO

---
+++ Build/Scripts/SyncMirror.ps1
@@ -0,0 +1,49 @@
+param(
+    [string] $SourceRepository,
+    [string] $TargetRepository,
+    [string] $Branch,
+    [switch] $ManagedIdentityAuth
+)
+
+Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
+
+function Get-AccessToken {
+    param(
+        [switch] $ManagedIdentityAuth
+    )
+
+    if ($ManagedIdentityAuth) {
+        az login --identity --allow-no-subscriptions | Out-Null
+    } else {
+        az login
+    }
+    return (az account get-access-token | ConvertFrom-Json)
+}
+
+$Branch = $Branch -replace "refs/heads/", ""
+$MIAccessToken = Get-AccessToken -ManagedIdentityAuth:$ManagedIdentityAuth
+
+git clone "https://$($MIAccessToken.accessToken)@$TargetRepository" BCApps
+Push-Location BCApps
+
+# Fetch repos and checkout branch
+RunAndCheck git reset HEAD --hard
+RunAndCheck git remote add upstream $SourceRepository
+RunAndCheck git fetch --all
+
+if (RunAndCheck git ls-remote origin $branch) {
+    RunAndCheck git checkout origin/$branch --track
+    RunAndCheck git pull origin $branch
+}
+else {
+    RunAndCheck git checkout upstream/$branch --track
+}
+
+# Merge changes from upstream
+RunAndCheck git pull upstream $branch
+
+# Push to origin
+RunAndCheck git push origin $Branch
+RunAndCheck git push origin --tags
+
+Pop-Location
