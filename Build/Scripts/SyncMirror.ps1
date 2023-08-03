23821bfcd9346452d6750fb272638a7c3c6b24d7
aholstrup1 <aholstrup@microsoft.com>
2023-08-03 16:09:39 +0200

    Sync mirror on new tags

--- Build/Scripts/SyncMirror.ps1
+++ Build/Scripts/SyncMirror.ps1
@@ -20,7 +20,6 @@ function Get-AccessToken {
     return (az account get-access-token | ConvertFrom-Json)
 }
 
-$Branch = $Branch -replace "refs/heads/", ""
 $MIAccessToken = Get-AccessToken -ManagedIdentityAuth:$ManagedIdentityAuth
 
 git clone "https://$($MIAccessToken.accessToken)@$TargetRepository" BCApps
@@ -31,22 +30,23 @@ RunAndCheck git reset HEAD --hard
 RunAndCheck git remote add upstream $SourceRepository
 RunAndCheck git fetch --all
 
-if (RunAndCheck git ls-remote origin $branch) {
-    RunAndCheck git checkout origin/$branch --track
-    RunAndCheck git pull origin $branch
+if ($Branch -match "refs/heads/") {
+    $Branch = $Branch -replace "refs/heads/", ""
+    if (RunAndCheck git ls-remote origin $branch) {
+        RunAndCheck git checkout origin/$branch --track
+        RunAndCheck git pull origin $branch
+    }
+    else {
+        RunAndCheck git checkout upstream/$branch --track
+    }
+    
+    # Merge changes from upstream
+    RunAndCheck git pull upstream $branch
+    
+    # Push to origin
+    RunAndCheck git push origin $Branch
 }
-else {
-    RunAndCheck git checkout upstream/$branch --track
-}
 
-# Merge changes from upstream
-RunAndCheck git pull upstream $branch
-
-# Push to origin
-RunAndCheck git push origin $Branch
-
-# Fetch latest tags and push to origin
-RunAndCheck git fetch --all --tags
 RunAndCheck git push origin --tags
 
 Pop-Location
