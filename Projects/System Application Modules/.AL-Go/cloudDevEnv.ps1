64ecaa147675c8e9284039558b1cd0eca5cba788
bcbuild-github-agent <137281497+bcbuild-github-agent@users.noreply.github.com>
2023-08-25 16:49:07 +0200

    Updated AL-Go System Files (#111)
    ## v3.2
    
    ### Issues
    
    Issue 542 Deploy Workflow fails
    Issue 558 CI/CD attempts to deploy from feature branch
    Issue 559 Changelog includes wrong commits
    Publish to AppSource fails if publisher name or app name contains
    national or special characters
    Issue 598 Cleanup during flush if build pipeline doesn't cleanup
    properly
    Issue 608 When creating a release, throw error if no new artifacts have
    been added
    Issue 528 Give better error messages when uploading to storage accounts
    Create Online Development environment workflow failed in AppSource
    template unless AppSourceCopMandatoryAffixes is defined in repository
    settings file
    Create Online Development environment workflow didn't have a project
    parameter and only worked for single project repositories
    Create Online Development environment workflow didn't work if runs-on
    was set to Linux
    Special characters are not supported in RepoName, Project names or other
    settings - Use UTF8 encoding to handle special characters in
    GITHUB_OUTPUT and GITHUB_ENV
    
    ### Issue 555
    AL-Go contains several workflows, which create a Pull Request or pushes
    code directly.
    All (except Update AL-Go System Files) earlier used the GITHUB_TOKEN to
    create the PR or commit.
    The problem using GITHUB_TOKEN is that is doesn't trigger a pull request
    build or a commit build.
    This is by design:
    https://docs.github.com/en/actions/using-workflows/triggering-a-workflow#triggering-a-workflow-from-a-workflow
    Now, you can set the checkbox called Use GhTokenWorkflow to allowing you
    to use the GhTokenWorkflow instead of the GITHUB_TOKEN - making sure
    that workflows are triggered
    
    ### New Settings
    - `keyVaultCodesignCertificateName`: With this setting you can delegate
    the codesigning to an Azure Key Vault. This can be useful if your
    certificate has to be stored in a Hardware Security Module
    - `PullRequestTrigger`: With this setting you can set which trigger to
    use for Pull Request Builds. By default AL-Go will use
    pull_request_target.
    
    ### New Actions
    - `DownloadProjectDependencies`: Downloads the dependency apps for a
    given project and build mode.
    
    ### Settings and Secrets in AL-Go for GitHub
    In earlier versions of AL-Go for GitHub, all settings were available as
    individual environment variables to scripts and overrides, this is no
    longer the case.
    Settings were also available as one compressed JSON structure in
    env:Settings, this is still the case.
    Settings can no longer contain line breaks. It might have been possible
    to use line breaks earlier, but it would likely have unwanted
    consequences.
    Use `$settings = $ENV:Settings | ConvertFrom-Json` to get all settings
    in PowerShell.
    
    In earlier versions of AL-Go for GitHub, all secrets requested by AL-Go
    for GitHub were available as individual environment variables to scripts
    and overrides, this is no longer the case.
    As described in bug 647, all secrets available to the workflow were also
    available in env:_Secrets, this is no longer the case.
    All requested secrets were also available (base64 encoded) as one
    compressed JSON structure in env:Secrets, this is still the case.
    Use `$secrets = $ENV:Secrets | ConvertFrom-Json` to get all requested
    secrets in PowerShell.
    You cannot get to any secrets that weren't requested by AL-Go for
    GitHub.
    
    Co-authored-by: mazhelez <mazhelez@users.noreply.github.com>

--- Projects/System Application Modules/.AL-Go/cloudDevEnv.ps1
+++ Projects/System Application Modules/.AL-Go/cloudDevEnv.ps1
@@ -17,10 +17,10 @@ $webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumen
 $webClient.Encoding = [System.Text.Encoding]::UTF8
 Write-Host "Downloading GitHub Helper module"
 $GitHubHelperPath = "$([System.IO.Path]::GetTempFileName()).psm1"
-$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go/d15c25e25c4b0033da9f99970e3a420e231f7fbc/Actions/Github-Helper.psm1', $GitHubHelperPath)
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v3.2/Github-Helper.psm1', $GitHubHelperPath)
 Write-Host "Downloading AL-Go Helper script"
 $ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
-$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go/d15c25e25c4b0033da9f99970e3a420e231f7fbc/Actions/AL-Go-Helper.ps1', $ALGoHelperPath)
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v3.2/AL-Go-Helper.ps1', $ALGoHelperPath)
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
