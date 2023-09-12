30f21443f87fba83447d67a319bc9b15262d1297
bcbuild-github-agent <137281497+bcbuild-github-agent@users.noreply.github.com>
2023-09-12 06:27:28 -0700

    Updated AL-Go System Files (#170)
    No release notes available!
    
    ---------
    
    Co-authored-by: mazhelez <mazhelez@users.noreply.github.com>
    Co-authored-by: Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>

--- Projects/System Application Modules/.AL-Go/localDevEnv.ps1
+++ Projects/System Application Modules/.AL-Go/localDevEnv.ps1
@@ -20,10 +20,10 @@ $webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumen
 $webClient.Encoding = [System.Text.Encoding]::UTF8
 Write-Host "Downloading GitHub Helper module"
 $GitHubHelperPath = "$([System.IO.Path]::GetTempFileName()).psm1"
-$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go/eabe480ba72260c6805e575f6bd03ac129367160/Actions/Github-Helper.psm1', $GitHubHelperPath)
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go/31be77d8466260c22e324516ec543285e6dfc521/Actions/Github-Helper.psm1', $GitHubHelperPath)
 Write-Host "Downloading AL-Go Helper script"
 $ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
-$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go/eabe480ba72260c6805e575f6bd03ac129367160/Actions/AL-Go-Helper.ps1', $ALGoHelperPath)
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go/31be77d8466260c22e324516ec543285e6dfc521/Actions/AL-Go-Helper.ps1', $ALGoHelperPath)
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
