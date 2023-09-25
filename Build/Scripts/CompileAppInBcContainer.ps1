6edee2a6ea464073ebdcbfc136949d4da5d816f3
mazhelez <mazhelez@microsoft.com>
2023-09-25 21:23:05 +0200

    Reuse PreCompileApp on CompileAppInBcContainer

--- Build/Scripts/CompileAppInBcContainer.ps1
+++ Build/Scripts/CompileAppInBcContainer.ps1
@@ -4,52 +4,7 @@ Param(
     [Hashtable] $parameters
 )
 
-Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
-
-$appBuildMode = Get-BuildMode
-
-# $app is a variable that determines whether the current app is a normal app (not test app, for instance)
-if($app)
-{
-    # Setup compiler features to generate captions and LCGs
-    if (!$parameters.ContainsKey("Features")) {
-        $parameters["Features"] = @()
-    }
-    $parameters["Features"] += @("generateCaptions")
-
-    # Setup compiler features to generate LCGs for the default build mode
-    if($appBuildMode -eq 'Default') {
-        $parameters["Features"] += @("lcgtranslationfile")
-    }
-
-    if($appBuildMode -eq 'Translated') {
-        Import-Module $PSScriptRoot\AppTranslations.psm1
-        Restore-TranslationsForApp -AppProjectFolder $parameters["appProjectFolder"]
-    }
-
-    # Restore the baseline app and generate the AppSourceCop.json file
-    if($gitHubActions) {
-        if (($parameters.ContainsKey("EnableAppSourceCop") -and $parameters["EnableAppSourceCop"]) -or ($parameters.ContainsKey("EnablePerTenantExtensionCop") -and $parameters["EnablePerTenantExtensionCop"])) {
-            Import-Module $PSScriptRoot\GuardingV2ExtensionsHelper.psm1
-
-            if($appBuildMode -eq 'Clean') {
-                Write-Host "Compile the app without any preprocessor symbols to generate a baseline app to use for breaking changes check"
-
-                $tempParameters = $parameters.Clone()
-
-                # Wipe the preprocessor symbols to ensure that the baseline is generated without any preprocessor symbols
-                $tempParameters["preprocessorsymbols"] = @()
-
-                # Place the app directly in the symbols folder
-                $tempParameters["appOutputFolder"] = $tempParameters["appSymbolsFolder"]
-
-                Compile-AppInBcContainer @tempParameters | Out-Null
-            }
-
-            Enable-BreakingChangesCheck -AppSymbolsFolder $parameters["appSymbolsFolder"] -AppProjectFolder $parameters["appProjectFolder"] -BuildMode $appBuildMode | Out-Null
-        }
-    }
-}
+. $PSScriptRoot\PreCompileApp.ps1 -parameters $parameters -currentProjectFolder $currentProjectFolder
 
 $appFile = Compile-AppInBcContainer @parameters
 
