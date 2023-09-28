80122e8cad12d1cb70eac3e115e2fbe4fe440b56
mazhelez <mazhelez@microsoft.com>
2023-09-28 13:23:28 +0200

    🤦‍♀️

--- Build/Scripts/PreCompileApp.ps1
+++ Build/Scripts/PreCompileApp.ps1
@@ -42,7 +42,7 @@ if($appType -eq 'app')
                 $tempParameters["preprocessorsymbols"] = @()
 
                 # Place the app directly in the sumbol folder
-                $tempParameters["appOutputFolder"] = Join $tempParameters["appProjectFolder"] '.baseline'
+                $tempParameters["appOutputFolder"] = Join-Path $tempParameters["appProjectFolder"] '.baseline'
                 if(-not (Test-Path $tempParameters["appOutputFolder"])) {
                     New-Item -ItemType Directory -Path $tempParameters["appOutputFolder"] | Out-Null
                 }
