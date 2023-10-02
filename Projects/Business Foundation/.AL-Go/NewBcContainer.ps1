9c3a3c5db7ea91d668b65ac7c993237920e56a3b
aholstrup1 <aholstrup@microsoft.com>
2023-10-02 16:02:13 +0200

    remove data from baseapp

--- Projects/Business Foundation/.AL-Go/NewBcContainer.ps1
+++ Projects/Business Foundation/.AL-Go/NewBcContainer.ps1
@@ -2,5 +2,21 @@ Param(
     [Hashtable]$parameters
 )
 
-$script = Join-Path $PSScriptRoot "../../../Build/Scripts/NewBcContainer.ps1" -Resolve
-. $script -parameters $parameters
+$parameters.multitenant = $false
+$parameters.RunSandboxAsOnPrem = $true
+if ("$env:GITHUB_RUN_ID" -eq "") {
+    $parameters.includeAL = $true
+    $parameters.doNotExportObjectsToText = $true
+    $parameters.shortcuts = "none"
+}
+
+New-BcContainer @parameters
+
+$installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
+$installedApps | ForEach-Object {
+    $removeData = $true
+    Write-Host "Removing $($_.Name)"
+    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData:$removeData -doNotSaveSchema:$removeData -force
+}
+
+Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
