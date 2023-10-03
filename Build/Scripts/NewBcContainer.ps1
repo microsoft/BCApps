45648abeda395679413042b957e55a67a8e82fb1
aholstrup1 <aholstrup@microsoft.com>
2023-10-03 19:21:53 +0200

    Syntax

--- Build/Scripts/NewBcContainer.ps1
+++ Build/Scripts/NewBcContainer.ps1
@@ -15,7 +15,7 @@ New-BcContainer @parameters
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
     Write-Host "Removing $($_.Name)"
-    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData: -doNotSaveSchema -force
+    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
