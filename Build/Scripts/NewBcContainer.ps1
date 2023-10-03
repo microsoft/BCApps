13ce30155a395d50e7c9d3edbd9513e748db3c25
aholstrup1 <aholstrup@microsoft.com>
2023-10-03 11:46:51 +0200

    New container - Do not save data

--- Build/Scripts/NewBcContainer.ps1
+++ Build/Scripts/NewBcContainer.ps1
@@ -14,9 +14,8 @@ New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
-    $removeData = $_.Name -ne "Base Application"
     Write-Host "Removing $($_.Name)"
-    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData:$removeData -doNotSaveSchema:$removeData -force
+    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData: -doNotSaveSchema -force
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
