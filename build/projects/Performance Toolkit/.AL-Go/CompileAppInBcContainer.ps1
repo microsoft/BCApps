49e8fd885b908f121dfcbce3e510aead60568939
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2023-10-05 07:08:34 +0200

    Move code around (#215)
    - projects to build/projects
    - build/Rulesets to src/rulesets
    - build/Scripts to build/scripts

--- build/projects/Performance Toolkit/.AL-Go/CompileAppInBcContainer.ps1
+++ build/projects/Performance Toolkit/.AL-Go/CompileAppInBcContainer.ps1
@@ -2,7 +2,7 @@ Param(
     [Hashtable] $parameters
 )
 
-$scriptPath = Join-Path $PSScriptRoot "../../../Build/Scripts/CompileAppInBcContainer.ps1" -Resolve
+$scriptPath = Join-Path $PSScriptRoot "../../../scripts/CompileAppInBcContainer.ps1" -Resolve
 $projectFolder = Join-Path $PSScriptRoot "../../Performance Toolkit" -Resolve
 
 . $scriptPath -parameters $parameters -currentProjectFolder $projectFolder
--- build/projects/System Application/.AL-Go/PreCompileApp.ps1
+++ build/projects/System Application/.AL-Go/PreCompileApp.ps1
@@ -3,7 +3,7 @@ Param(
     [ref] $compilationParams
 )
 
-$scriptPath = Join-Path $PSScriptRoot "../../../Build/Scripts/PreCompileApp.ps1" -Resolve
-$projectFolder = Join-Path $PSScriptRoot "../../Test Stability Tools"
+$scriptPath = Join-Path $PSScriptRoot "../../../scripts/PreCompileApp.ps1" -Resolve
+$projectFolder = Join-Path $PSScriptRoot "../../System Application"
 
 . $scriptPath -parameters $compilationParams -currentProjectFolder $projectFolder -appType $appType
