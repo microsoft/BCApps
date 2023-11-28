7eba65aba9decbd38868c5f454f8eb22fad464b1
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-11-28 07:58:28 +0100

    Update workitem validation logic (#365)
    <!-- Thank you for submitting a Pull Request. If you're new to
    contributing to BCApps please read our pull request guideline below
    * https://github.com/microsoft/BCApps/Contributing.md
    -->
    #### Summary <!-- Provide a general summary of your changes -->
    
    Updated the workitem validation such that
    * All PRs from forks must reference a GitHub issue 
    * All PRs must reference an ADO workitem (regardless of whether they're
    from a fork or not)
    
    To help link BCIdea workitems to pull requests, I've added a stage to
    the WorkitemValidation.yaml that looks for ADO workitems in the linked
    issues and automatically links them to the pull request.
    
    #### Work Item(s) <!-- Add the issue number here after the #. The issue
    needs to be open and approved. Submitting PRs with no linked issues or
    unapproved issues is highly discouraged. -->
    Fixes AB#476348
    
    ---------
    
    Co-authored-by: aholstrup1@microsoft.com <aholstrup1@microsoft.com@users.noreply.github.com>
    Co-authored-by: Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>

---
+++ build/scripts/PullRequestValidation/ValidateInternalWorkItemForPullRequest.ps1
@@ -0,0 +1,56 @@
+using module ..\GitHub\GitHubPullRequest.class.psm1
+using module ..\GitHub\GitHubIssue.class.psm1
+
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $PullRequestNumber,
+    [Parameter(Mandatory = $true)]
+    [string] $Repository
+)
+
+# Set error action
+$ErrorActionPreference = "Stop"
+
+<#
+    .Synopsis
+    Validates that the pull request description contains a line that links the pull request to an ADO workitem.
+    .Parameter ADOWorkItems
+    The IDs of the ADO workitems linked to the pull request.
+    .Parameter PullRequest
+    The pull request to validate.
+#>
+function Test-ADOWorkItemIsLinked() {
+    param(
+        [Parameter(Mandatory = $false)]
+        [string[]] $ADOWorkItems,
+        [Parameter(Mandatory = $false)]
+        [object] $PullRequest
+    )
+
+    $Comment = "Could not find a linked ADO workitem. Please link one by using the pattern 'Fixes AB#' followed by the workitem number being fixed."
+    if (-not $ADOWorkItems) {
+        # If the pull request is not from a fork, add a comment to the pull request
+        if (-not $PullRequest.IsFromFork()) {
+            $PullRequest.AddComment($Comment)
+        }
+
+        # Throw an error if there is no linked ADO workitem
+        throw $Comment
+    }
+
+    $PullRequest.RemoveComment($Comment)
+}
+
+Write-Host "Validating PR $PullRequestNumber"
+
+$pullRequest = [GitHubPullRequest]::Get($PullRequestNumber, $Repository)
+if (-not $pullRequest) {
+    throw "Could not get PR $PullRequestNumber from repository $Repository"
+}
+
+$adoWorkItems = $pullRequest.GetLinkedADOWorkItemIDs()
+
+# Validate that all pull requests links to an ADO workitem
+Test-ADOWorkItemIsLinked -ADOWorkItems $adoWorkItems -PullRequest $PullRequest
+
+Write-Host "PR $PullRequestNumber validated successfully" -ForegroundColor Green
