4633007218c490d23a0e6e252639b30600408596
aholstrup1 <aholstrup1@microsoft.com@users.noreply.github.com>
2023-11-30 09:05:30 +0100

    Rename to AppFolder

--- build/projects/System Application Modules/.AL-Go/PreCompileApp.ps1
+++ build/projects/System Application Modules/.AL-Go/PreCompileApp.ps1
@@ -8,5 +8,5 @@ if($appType -eq 'app')
     Write-Host "compilationParams: $compilationParams"
 
     $scriptPath = Join-Path $PSScriptRoot "../../../scripts/VerifyExecutePermissions.ps1" -Resolve
-    . $scriptPath -ModulesDirectory $compilationParams.Value["appProjectFolder"]
+    . $scriptPath -AppFolder $compilationParams.Value["appProjectFolder"]
 }
