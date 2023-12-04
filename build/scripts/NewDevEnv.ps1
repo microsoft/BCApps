724e8a5a230b767bcd284dec8f8979acdbac7c35
mazhelez <mazhelez@microsoft.com>
2023-12-04 23:53:37 +0100

    WIP

--- build/scripts/NewDevEnv.ps1
+++ build/scripts/NewDevEnv.ps1
@@ -1,9 +1,10 @@
 <#
-    Creates a new dev env.
+Creates a new dev env.
 #>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'local build')]
 param(
     [Parameter(Mandatory = $false)]
-    [string] $containerName = "BC-$(Get-Date -Format 'yyyyMMdd-HHmm')",
+    [string] $containerName = "BC-$(Get-Date -Format 'yyyyMMdd')",
 
     [Parameter(Mandatory = $false)]
     [string] $userName = 'admin',
@@ -14,7 +15,6 @@ param(
     [Parameter(Mandatory = $true)]
     [string[]] $projects
 )
-
 $errorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
 Import-Module "$PSScriptRoot/EnlistmentHelperFunctions.psm1"
 
@@ -22,23 +22,47 @@ if (-not (Get-Module -ListAvailable -Name "BCContainerHelper")) {
     Write-Host "BCContainerHelper module not found. Installing..."
     Install-Module -Name "BCContainerHelper" -Scope CurrentUser -AllowPrerelease -Force
 }
+$containerExist = $null -ne $(docker ps -q -f name="$containerName")
 
-$bcArtifactUrl = Get-ConfigValue -Key "artifact" -ConfigType AL-Go
+if (-not $containerExist) {
+    Write-Host "Creating container $containerName"
 
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', Justification = 'local build')]
-$credential = New-Object -TypeName pscredential -ArgumentList $userName, $password
+    $bcArtifactUrl = Get-ConfigValue -Key "artifact" -ConfigType AL-Go
+    $credential = New-Object -TypeName pscredential -ArgumentList $userName, $password
 
-New-BcContainer `
-    -accept_eula `
-    -containerName 'test' `
-    -artifactUrl $bcArtifactUrl `
-    -accept_insiderEula `
-    -auth UserPassword `
-    -Credential $credential
+    $newContainerParams = @{
+        "accept_eula" = $true
+        "accept_insiderEula" = $true
+        "containerName" = "BC-$(Get-Date -Format 'yyyyMMdd')"
+        "artifactUrl" = $bcArtifactUrl
+        "Credential" = $credential
+        "auth" = "UserPassword"
+        "additionalParameters" = @("--volume ""$(Get-BaseFolder):c:\sources""")
+    }
+
+    New-BcContainer @newContainerParams
+}
 
 $projects | ForEach-Object {
     $currentProject = $_
 
-    if(Test-Pa)
+    if(-not (Test-Path -Path (Join-Path $currentProject 'app.json'))) {
+        Write-Host "app.json not found in $currentProject. Skipping..."
+        return
+    }
+
+    $compilationParameters = @{
+        "containerName" = $containerName
+        "tenant" = $tenant
+        "credential" = $credential
+        "appFile" = $appFile
+        "skipVerification" = $true
+        "sync" = $true
+        "install" = $true
+        "useDevEndpoint" = $useDevEndpoint
+    }
+
+    Write-Host "Adding $currentProject to $containerName"
+    Compile-AppInBcContainer -containerName $containerName -projectPath $currentProject
 }
 Write-Host
