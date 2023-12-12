27cc9dd79e9522d6db27b1fdf1eb2585fca71177
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-12-12 13:29:26 +0100

    Update AL-Go System Files (#425)
    <!-- Thank you for submitting a Pull Request. If you're new to
    contributing to BCApps please read our pull request guideline below
    * https://github.com/microsoft/BCApps/Contributing.md
    -->
    #### Summary <!-- Provide a general summary of your changes -->
    Update AL-GO System files
    
    #### Work Item(s) <!-- Add the issue number here after the #. The issue
    needs to be open and approved. Submitting PRs with no linked issues or
    unapproved issues is highly discouraged. -->
    Fixes AB#420000
    
    ---------
    
    Co-authored-by: aholstrup1 <aholstrup1@microsoft.com@users.noreply.github.com>
    Co-authored-by: aholstrup1 <aholstrup1@users.noreply.github.com>

--- build/projects/Test Framework/.AL-Go/cloudDevEnv.ps1
+++ build/projects/Test Framework/.AL-Go/cloudDevEnv.ps1
@@ -27,11 +27,11 @@ Write-Host -ForegroundColor Yellow @'
 $webClient = New-Object System.Net.WebClient
 $webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumentList ([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore)
 $webClient.Encoding = [System.Text.Encoding]::UTF8
-$GitHubHelperUrl = 'https://raw.githubusercontent.com/microsoft/AL-Go/4aa78c89b0c0c3a54c38d2815dfae789239db8b7/Actions/Github-Helper.psm1'
+$GitHubHelperUrl = 'https://raw.githubusercontent.com/microsoft/AL-Go/fc491ffce4cc911d677b3b4318a9372fbe54141e/Actions/Github-Helper.psm1'
 Write-Host "Downloading GitHub Helper module from $GitHubHelperUrl"
 $GitHubHelperPath = "$([System.IO.Path]::GetTempFileName()).psm1"
 $webClient.DownloadFile($GitHubHelperUrl, $GitHubHelperPath)
-$ALGoHelperUrl = 'https://raw.githubusercontent.com/microsoft/AL-Go/4aa78c89b0c0c3a54c38d2815dfae789239db8b7/Actions/AL-Go-Helper.ps1'
+$ALGoHelperUrl = 'https://raw.githubusercontent.com/microsoft/AL-Go/fc491ffce4cc911d677b3b4318a9372fbe54141e/Actions/AL-Go-Helper.ps1'
 Write-Host "Downloading AL-Go Helper script from $ALGoHelperUrl"
 $ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
 $webClient.DownloadFile($ALGoHelperUrl, $ALGoHelperPath)
