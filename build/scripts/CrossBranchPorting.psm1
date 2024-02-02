de73e6565e46a32457ba2551d0a66a0e5c6b6c4d
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2024-02-02 10:12:24 +0100

    Update CrossBranchPorting.psm1

--- build/scripts/CrossBranchPorting.psm1
+++ build/scripts/CrossBranchPorting.psm1
@@ -19,13 +19,13 @@ function New-BCAppsBackport() {
 
     # Get the pull request details
     $pullRequestDetails = (gh pr view $PullRequestNumber --json title,number | ConvertFrom-Json)
-    
+
     # Get the list of existing pull requests
     $existingPullRequests = gh pr list --json title,state,baseRefName,url | ConvertFrom-Json
 
     # Get the current branch before starting to backport
     $startingBranch = RunAndCheck git rev-parse --abbrev-ref HEAD
-    
+
     $pullRequests = @{}
     $branchNameSuffix = "$PullRequestNumber/$(Get-Date -Format "yyyyMMddHHmmss")"
     foreach($TargetBranch in $TargetBranches) {
@@ -40,7 +40,7 @@ function New-BCAppsBackport() {
                 Write-Host "Pull request for $TargetBranch already exists: $($existingPr.url)" -ForegroundColor Yellow
                 continue
             }
-            
+
             # Create a new branch for the cherry-pick
             $cherryPickBranch = "hotfix/$TargetBranch/$branchNameSuffix"
             RunAndCheck git checkout -b $cherryPickBranch $TargetBranch
@@ -52,14 +52,14 @@ function New-BCAppsBackport() {
             # Push the branch and create a pull request
             RunAndCheck git push origin $cherryPickBranch
             gh pr create --title $title --body $body --base $TargetBranch --head $cherryPickBranch
-    
+
             $backportPr = gh pr view --json url | ConvertFrom-Json
             if ($backportPr.url) {
                 $pullRequests.Add($TargetBranch, $backportPr.url)
             }
         } catch {
             git am --abort
-            RunAndCheck git checkout $startingBranch 
+            RunAndCheck git checkout $startingBranch
             Write-Host "Failed to backport to $TargetBranch. Please inspect the error and try again." -ForegroundColor Red
             Write-Host $_.Exception.Message -ForegroundColor Red
             throw $_.Exception.Message
@@ -73,7 +73,7 @@ function New-BCAppsBackport() {
         Write-Host "No pull requests created" -ForegroundColor Yellow
         return
     }
-    
+
     Write-Host "Backport pull requests created:" -ForegroundColor Green
     foreach($pullRequest in $pullRequests.GetEnumerator()) {
         Write-Host " - $($pullRequest.Key): $($pullRequest.Value)" -ForegroundColor Green
