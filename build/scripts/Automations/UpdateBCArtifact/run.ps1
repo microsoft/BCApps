b3c4d664654e7e0d93faefae500028f2eb396816
mazhelez <mazhelez@microsoft.com>
2024-04-09 12:39:25 +0200

    🤦‍♀️

--- build/scripts/Automations/UpdateBCArtifact/run.ps1
+++ build/scripts/Automations/UpdateBCArtifact/run.ps1
@@ -1,12 +1,9 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'runParameters', Justification = 'The parameter is always passed to the script')]
 param (
     [Parameter(Mandatory=$true)]
-    $[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'runParameters', Justification = 'The parameter is always passed to the script')]
-    param (
-        [Parameter(Mandatory=$true)]
-        $runParameters
-    )
+    $runParameters
 )
+
 
 # BC Container Helper is needed to fetch the latest artifact version
 Install-Module -Name BcContainerHelper -AllowPrerelease -Force
