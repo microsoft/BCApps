7f82ead1a12af75f0d44d40397f0b5546b429038
mazhelez <mazhelez@microsoft.com>
2024-04-19 09:39:44 +0200

    Fix typo

--- .github/actions/RunAutomation/UpdateAppBaselines/run.ps1
+++ .github/actions/RunAutomation/UpdateAppBaselines/run.ps1
@@ -17,7 +17,7 @@ $result = @{
 }
 
 if ($newVersion) {
-    $result.Files = @(Get=PackagesFilePath -Relative)
+    $result.Files = @(Get-PackagesFilePath -Relative)
     $result.Message = "Update app baselines package version. New value: $newVersion"
 }
 
