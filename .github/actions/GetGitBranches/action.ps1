555f9357f75ec5f459e1607858eacb59a928951e
mazhelez <mazhelez@microsoft.com>
2024-04-19 09:23:33 +0200

    Fix PS module name (copilot issue :D)

--- .github/actions/GetGitBranches/action.ps1
+++ .github/actions/GetGitBranches/action.ps1
@@ -5,7 +5,7 @@ param (
 
 # Current path is .github/actions/GetGitBranches
 # Import EnlistmentHelperFunctions module from build/scripts
-Import-Module "$PSScriptRoot\..\..\..\build\scripts\EnlistmentHelper.psm1" -DisableNameChecking
+Import-Module "$PSScriptRoot\..\..\..\build\scripts\EnlistmentHelperFunctions.psm1" -DisableNameChecking
 
 $includeBranches = ConvertFrom-Json $include
 
