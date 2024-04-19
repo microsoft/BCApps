c32a5b8df4136a682b10588e3a930a88feb1eaf0
mazhelez <mazhelez@microsoft.com>
2024-04-19 08:59:38 +0200

    Use RunAndCheck for the git opearations

--- .github/actions/GetGitBranches/action.ps1
+++ .github/actions/GetGitBranches/action.ps1
@@ -3,10 +3,14 @@ param (
     [string] $include = '[]'
 )
 
+# Current path is .github/actions/GetGitBranches
+# Import EnlistmentHelperFunctions module from build/scripts
+Import-Module "$PSScriptRoot\..\..\..\build\scripts\EnlistmentHelper.psm1" -DisableNameChecking
+
 $includeBranches = ConvertFrom-Json $include
 
-git fetch
-$allBranches = @(git for-each-ref --format="%(refname:short)" refs/remotes/origin/) | Where-Object { $_ -ne 'origin' } | ForEach-Object { $_ -replace 'origin/', '' }
+RunAndCheck git fetch
+$allBranches = @(RunAndCheck git for-each-ref --format="%(refname:short)" refs/remotes/origin/) | Where-Object { $_ -ne 'origin' } | ForEach-Object { $_ -replace 'origin/', '' }
 
 if ($includeBranches) {
     Write-Host "Filtering branches by: $($includeBranches -join ', ')"
