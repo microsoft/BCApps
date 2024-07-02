6b81bbb767fef1f35bd5d061665bef4c3c43d31d
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2024-07-02 16:56:16 +0200

    Updated devcontainer experience (#825)
    <!-- Thank you for submitting a Pull Request. If you're new to
    contributing to BCApps please read our pull request guideline below
    * https://github.com/microsoft/BCApps/Contributing.md
    -->
    #### Summary <!-- Provide a general summary of your changes -->
    This PR would add a script to set up a dev container for BCApps. The
    script uses containerhelper to set up a standard container. Once a
    container is set up, all the apps are moved to dev scope and the AL
    projects in the repo get a .vscode folder generated with a launch.json
    and settings.json
    
    #### Work Item(s) <!-- Add the issue number here after the #. The issue
    needs to be open and approved. Submitting PRs with no linked issues or
    unapproved issues is highly discouraged. -->
    Fixes
    [AB#521408](https://dynamicssmb2.visualstudio.com/1fcb79e7-ab07-432a-a3c6-6cf5a88ba4a5/_workitems/edit/521408)

--- build/scripts/DevEnv/NewDevEnv.ps1
+++ build/scripts/DevEnv/NewDevEnv.ps1
@@ -1,110 +1,132 @@
-using module .\AppProjectInfo.class.psm1
-using module .\ALGoProjectInfo.class.psm1
-
 <#
-    .Synopsis
-        Creates a docker-based development environment for AL apps.
-    .Parameter containerName
-        The name of the container to use. The container will be created if it does not exist.
-    .Parameter userName
-        The user name to use for the container.
-    .Parameter password
-        The password to use for the container.
-    .Parameter projectPaths
-        The paths of the AL projects to build. May contain wildcards.
-    .Parameter workspacePath
-        The path of the workspace to build. The workspace file must be in JSON format.
-    .Parameter alGoProject
-        The path of the AL-Go project to build.
-    .Parameter packageCacheFolder
-        The folder to store the built artifacts.
+    .SYNOPSIS
+    Creates a new development environment with a new container, sets up vscode for development against the container, and optionally builds and publishes apps.
+    .DESCRIPTION
+    This script creates a new development environment with a new container, sets up vscode for development against the container, and optionally builds and publishes apps.
+    .PARAMETER ContainerName
+    The name of the container to create. The default value is "BC-$(Get-Date -Format 'yyyyMMdd')".
+    .PARAMETER Authentication
+    The authentication type to use when creating the container. The default value is "UserPassword".
+    .PARAMETER SkipVsCodeSetup
+    If specified, vscode will not be set up for development against the container.
+    .PARAMETER ProjectPaths
+    The paths to the AL projects to build and publish. This parameter is mutually exclusive with the WorkspacePath and AlGoProject parameters.
+    .PARAMETER WorkspacePath
+    The path to the workspace containing the AL projects to build and publish. This parameter is mutually exclusive with the ProjectPaths and AlGoProject parameters.
+    .PARAMETER AlGoProject
+    The name of the AL-Go project to build and publish. This parameter is mutually exclusive with the ProjectPaths and WorkspacePath parameters.
+    .PARAMETER RebuildApps
+    If specified, the apps will be force rebuilt before publishing even if they exist.
 #>
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', '', Justification = 'local build')]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'local build')]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'packageCacheFolder', Justification = 'false-postiive, used in Measure-Command')]
-[CmdletBinding(DefaultParameterSetName = 'ProjectPaths')]
+
+[CmdletBinding()]
 param(
     [Parameter(Mandatory = $false)]
-    [string] $containerName = "BC-$(Get-Date -Format 'yyyyMMdd')",
-
+    [string] $ContainerName = "BC-$(Get-Date -Format 'yyyyMMdd')",
     [Parameter(Mandatory = $false)]
-    [string] $userName = 'admin',
-
-    [Parameter(Mandatory = $true)]
-    [string] $password,
-
-    [Parameter(Mandatory = $true, ParameterSetName = 'ProjectPaths')]
-    [string[]] $projectPaths,
-
-    [Parameter(Mandatory = $true, ParameterSetName = 'WorkspacePath')]
-    [string] $workspacePath,
-
-    [Parameter(Mandatory = $true, ParameterSetName = 'ALGoProject')]
-    [string] $alGoProject,
-
+    [ValidateSet('UserPassword')]
+    [string] $Authentication = "UserPassword",
     [Parameter(Mandatory = $false)]
-    [string] $packageCacheFolder = ".artifactsCache"
+    [switch] $SkipVsCodeSetup,
+    [Parameter(Mandatory = $false)]
+    [string[]] $ProjectPaths,
+    [Parameter(Mandatory = $false)]
+    [string] $WorkspacePath,
+    [Parameter(Mandatory = $false)]
+    [string] $AlGoProject,
+    [Parameter(Mandatory = $false)]
+    [switch] $RebuildApps
 )
 
-$errorActionPreference = "Stop"; $ProgressPreference = "SilentlyContinue"; Set-StrictMode -Version 2.0
-
-# Install BCContainerHelper module if not already installed
-if (-not (Get-Module -ListAvailable -Name "BCContainerHelper")) {
-    Write-Host "BCContainerHelper module not found. Installing..."
-    Install-Module -Name "BCContainerHelper" -Scope CurrentUser -AllowPrerelease -Force
-}
-
-Import-Module "BCContainerHelper" -DisableNameChecking
 Import-Module "$PSScriptRoot\..\EnlistmentHelperFunctions.psm1" -DisableNameChecking
+Import-Module "$PSScriptRoot\ALDev.psm1" -DisableNameChecking
 Import-Module "$PSScriptRoot\NewDevEnv.psm1" -DisableNameChecking
+Import-Module BcContainerHelper
 
-$baseFolder = Get-BaseFolder
+$baseFolder = (Get-BaseFolderForPath -Path $PSScriptRoot)
+Push-Location $baseFolder
 
-# Create BC container
-$credential = New-Object System.Management.Automation.PSCredential ($userName, $(ConvertTo-SecureString $password -AsPlainText -Force))
-$createContainerJob = Create-BCContainer -containerName $containerName -credential $credential -backgroundJob
+try {
+    $credential = Get-CredentialForContainer -AuthenticationType $Authentication
 
-# Resolve AL project paths
-$projectPaths = Resolve-ProjectPaths -projectPaths $projectPaths -workspacePath $workspacePath -alGoProject $alGoProject -baseFolder $baseFolder
-Write-Host "Resolved project paths: $($projectPaths -join [Environment]::NewLine)"
+    # Create the container if it does not exist already
+    $ContainerExists = Test-ContainerExists -ContainerName $ContainerName
+    if (-not $ContainerExists)
+    {
+        Write-Host "Creating container $ContainerName..." -ForegroundColor Yellow
+        $createContainerJob = Create-BCContainer -ContainerName $ContainerName -Authentication $Authentication -Credential $credential -backgroundJob
+    }
 
-# Build apps
-$appFiles = @()
-$buildingAppsStats = Measure-Command {
-    Write-Host "Building apps..." -ForegroundColor Yellow
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'false-postiive')]
-    $appFiles = Build-Apps -projectPaths $projectPaths -packageCacheFolder $packageCacheFolder
-}
+    # Build the apps
+    if ($ProjectPaths -or $WorkspacePath -or $AlGoProject)
+    {
+        # Resolve AL project paths
+        $ProjectPaths = Resolve-ProjectPaths -ProjectPaths $ProjectPaths -WorkspacePath $WorkspacePath -AlGoProject $AlGoProject
 
-Write-Host "Building apps took $($buildingAppsStats.TotalSeconds) seconds"
-Write-Host "App files: $($appFiles -join [Environment]::NewLine)" -ForegroundColor Green
+        # Build apps
+        Write-Host "Building apps..." -ForegroundColor Yellow
+        $buildAppFiles = Build-Apps -ProjectPaths $ProjectPaths -packageCacheFolder (Get-ArtifactsCacheFolder -ContainerName $ContainerName) -rebuild:$RebuildApps
+    }
 
-# Wait for container creation to finish
-if($createContainerJob) {
-    Write-Host 'Waiting for container creation to finish...' -ForegroundColor Yellow
-    Wait-Job -Job $createContainerJob -Timeout 1
-    Receive-Job -Job $createContainerJob -Wait -AutoRemoveJob
+    # Wait for container creation to finish
+    if($createContainerJob) {
+        Write-Host 'Waiting for container creation to finish...' -ForegroundColor Yellow
+        Wait-Job -Job $createContainerJob -Timeout 1
+        Receive-Job -Job $createContainerJob -Wait -AutoRemoveJob
 
-    if($createContainerJob.State -eq 'Failed'){
-        Write-Output "Creating container failed:"
-        throw $($createContainerJob.ChildJobs | ForEach-Object { $_.JobStateInfo.Reason.Message } | Out-String)
+        if($createContainerJob.State -eq 'Failed'){
+            Write-Output "Creating container failed:"
+            throw $($createContainerJob.ChildJobs | ForEach-Object { $_.JobStateInfo.Reason.Message } | Out-String)
+        }
+    }
+
+    # Publish apps
+    if ($buildAppFiles) {
+        Write-Host "Publishing apps..." -ForegroundColor Yellow
+        $appPublishingResults = Publish-Apps -ContainerName $ContainerName -AppFiles $buildAppFiles -Credential $credential
+    }
+
+    # Set up vscode for development against the container (i.e. set up launch.json and settings.json)
+    if (-not $SkipVsCodeSetup)
+    {
+        Write-Host "Configuring vscode for development against the container..."
+        Configure-ALProjectsInPath -ContainerName $ContainerName -Authentication $Authentication
     }
 }
+finally {
+    Pop-Location
 
-if(Test-ContainerExists -containerName $containerName) {
-    Write-Host "Container $containerName is available" -ForegroundColor Green
-} else {
-    throw "Container $containerName not available. Check if the container was created successfully and is running."
-}
+    # Output results
+    if ($createContainerJob) {
+        if ($createContainerJob.State -eq 'Failed') {
+            Write-Host "Container $ContainerName failed to be created." -ForegroundColor Red
+        } else {
+            Write-Host "Container $ContainerName created successfully. You can access the webclient by going to http://$ContainerName/BC/ in your browser" -ForegroundColor Green
+        }
+    } else {
+        Write-Host "Skipped creating container as it already exists. You can access the webclient by going to http://$ContainerName/BC/ in your browser" -ForegroundColor Yellow
+    }
 
+    if ($buildAppFiles) {
+        # Output the app files that were built. One per line
+        Write-Host "Apps built successfully:" -ForegroundColor Green
+        $buildAppFiles | ForEach-Object { Write-Host $_ -ForegroundColor Green }
+    } else {
+        Write-Host "Skipped building apps." -ForegroundColor Yellow
+    }
 
-# Publish apps
-Write-Host "Publishing apps..." -ForegroundColor Yellow
-$publishingAppsStats = Measure-Command {
-    foreach($currentAppFile in $appFiles) {
-        Publish-BcContainerApp -containerName $containerName -appFile $currentAppFile -syncMode ForceSync -sync -credential $credential -skipVerification -install -useDevEndpoint -replacePackageId
+    if ($appPublishingResults) {
+        # Output the app files that were published. One per line
+        if ($appPublishingResults | Where-Object { $_.Success }) {
+            Write-Host "Apps published successfully:" -ForegroundColor Green
+            $appPublishingResults | Where-Object { $_.Success } | ForEach-Object { Write-Host $_.AppFile -ForegroundColor Green }
+        }
+
+        if ($appPublishingResults | Where-Object { -not $_.Success }) {
+            Write-Host "Apps failed to publish:" -ForegroundColor Red
+            $appPublishingResults | Where-Object { -not $_.Success } | ForEach-Object { Write-Host $_.AppFile -ForegroundColor Red }
+        }
+    } else {
+        Write-Host "Skipped publishing apps." -ForegroundColor Yellow
     }
 }
-
-Write-Host "Publishing apps took $($publishingAppsStats.TotalSeconds) seconds"
-
