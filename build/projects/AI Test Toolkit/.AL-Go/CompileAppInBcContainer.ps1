80e849ebf83e7b9377bd96fc40c56045e17a99bc
Nikola Kukrika <nikolak@microsoft.com>
2024-07-05 11:51:18 +0200

    Adding build scripts

---
+++ build/projects/AI Test Toolkit/.AL-Go/CompileAppInBcContainer.ps1
@@ -0,0 +1,8 @@
+Param(
+    [Hashtable] $parameters
+)
+
+$scriptPath = Join-Path $PSScriptRoot "../../../scripts/CompileAppInBcContainer.ps1" -Resolve
+$projectFolder = Join-Path $PSScriptRoot "../../AI Test Toolkit" -Resolve
+
+. $scriptPath -parameters $parameters -currentProjectFolder $projectFolder
