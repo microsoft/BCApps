9b29bc15cdd2774a0c1ff55dc3cf11e9b7e705ea
mazhelez <mazhelez@microsoft.com>
2024-07-06 13:02:53 +0200

    Fix AI Toolkit AL-Go project settings

--- build/projects/AI Test Toolkit/.AL-Go/CompileAppInBcContainer.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-    [Hashtable] $parameters
-)
-
-$scriptPath = Join-Path $PSScriptRoot "../../../scripts/CompileAppInBcContainer.ps1" -Resolve
-$projectFolder = Join-Path $PSScriptRoot "../../AI Test Toolkit" -Resolve
-
-. $scriptPath -parameters $parameters -currentProjectFolder $projectFolder
