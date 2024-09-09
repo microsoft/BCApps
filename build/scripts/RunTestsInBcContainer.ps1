619faca3fbd401aaefd0a7bd7004df52da5f4b31
mazhelez <mazhelez@microsoft.com>
2024-09-09 23:02:29 +0200

    Manually disable retention policy tests when running with test isolation

--- build/scripts/RunTestsInBcContainer.ps1
+++ build/scripts/RunTestsInBcContainer.ps1
@@ -25,4 +25,42 @@ if ($disabledTests)
     $parameters["disabledTests"] = $disabledTests
 }
 
+if ($parameters["testRunnerCodeunitId"] -ne "138705")
+{
+    # The follwoing test codeunits need to run without test isolation
+    $retentionPolicyTestCodeunits = @(
+        @{
+            "codeunitId" = "138700"
+            "codeunitName" = "Retention Period Test"
+        },
+        @{
+            "codeunitId" = "138701"
+            "codeunitName" = "Reten. Policy Setup Test"
+        },
+        @{
+            "codeunitId" = "138702"
+            "codeunitName" = "Retention Policy Test"
+        },
+        @{
+            "codeunitId" = "138703"
+            "codeunitName" = "Reten. Pol. Allowed Tbl. Test"
+        },
+        @{
+            "codeunitId" = "138705"
+            "codeunitName" = "Retention Policy Log Test"
+        }
+    )
+
+    $retentionPolicyTestCodeunits | ForEach-Object {
+        # Manually disable the test codeunit
+        $disabledTests += @{
+            "codeunitId" = $_.codeunitId
+            "codeunitName" = $_.codeunitName
+            "method" = "*"
+        }
+    }
+}
+
+Write-Host "Disabled tests: $($disabledTests | ConvertTo-Json -Depth 100)"
+
 Run-TestsInBcContainer @parameters
