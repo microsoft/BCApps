2e61876eaa1a37b770f62f91b14fd131a5f4f5d7
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2024-09-11 12:22:15 +0200

    Run Retention Policy tests with no test isolation (#1989)

--- build/scripts/RunTestsInBcContainer.ps1
+++ build/scripts/RunTestsInBcContainer.ps1
@@ -1,5 +1,6 @@
 Param(
-    [Hashtable]$parameters
+    [Hashtable] $parameters,
+    [switch] $DisableTestIsolation
 )
 
 Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
@@ -18,7 +19,45 @@ function Get-DisabledTests
     return @($disabledTests)
 }
 
-$disabledTests = Get-DisabledTests
+function Get-TestsInGroup {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $groupName
+    )
+
+    $baseFolder = Get-BaseFolder
+
+    $groupFiles = Get-ChildItem -Path $baseFolder -Filter 'TestGroups.json' -Recurse -File
+
+    $testsInGroup = @()
+    foreach($groupFile in $groupFiles)
+    {
+        $testsInGroup += Get-Content -Raw -Path $groupFile.FullName | ConvertFrom-Json | Where-Object { $_.group -eq $groupName }
+    }
+
+    return $testsInGroup
+}
+
+$disabledTests = @(Get-DisabledTests)
+$noIsolationTests = Get-TestsInGroup -groupName "No Test Isolation"
+
+if ($DisableTestIsolation)
+{
+    $parameters["testRunnerCodeunitId"] = "130451" # Test Runner with disabled test isolation
+
+    # When test isolation is disabled, only tests from the "No Test Isolation" group should be run
+    $parameters["testCodeunitRange"] = @($noIsolationTests | ForEach-Object { $_.codeunitId }) -join "|"
+}
+else { # Test isolation is enabled
+    # Manually disable the test codeunits, as they need to be run without test isolation
+    $noIsolationTests | ForEach-Object {
+        $disabledTests += @{
+            "codeunitId" = $_.codeunitId
+            "codeunitName" = $_.codeunitName
+            "method" = "*"
+        }
+    }
+}
 
 if ($disabledTests)
 {
