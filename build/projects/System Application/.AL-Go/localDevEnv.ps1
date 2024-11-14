771d6145ad94afe1d2748383d302468baab52528
bcbuild-github-agent <137281497+bcbuild-github-agent@users.noreply.github.com>
2024-11-14 02:26:39 -0800

    [releases/24.4] Update AL-Go System Files from microsoft/AL-Go-PTE@preview -  50903624159826257124c0f799a1b4add9b0260b (#2352)
    ## v6.1
    
    ### Issues
    
    - Issue 1241 Increment Version Number might produce wrong app.json
    - When auto discovering appFolders, testFolders and bcptTestFolders - if
    a BCPT Test app has a dependency to a test framework app, it is added to
    testFolders as well as bcptTestFolders and will cause a failure.
    
    ### New Project Settings
    
    - `pageScriptingTests` should be an array of page scripting test file
    specifications, relative to the AL-Go project. Examples of file
    specifications: `recordings/my*.yml` (for all yaml files in the
    recordings subfolder matching my\*.yml), `recordings` (for all \*.yml
    files in the recordings subfolder) or `recordings/test.yml` (for a
    single yml file)
    - `doNotRunPageScriptingTests` can force the pipeline to NOT run the
    page scripting tests specified in pageScriptingTests. Note this setting
    can be set in a [workflow specific settings
    file](#where-are-the-settings-located) to only apply to that workflow
    - `restoreDatabases` should be an array of events, indicating when you
    want to start with clean databases in the container. Possible events
    are: `BeforeBcpTests`, `BeforePageScriptingTests`, `BeforeEachTestApp`,
    `BeforeEachBcptTestApp`, `BeforeEachPageScriptingTest`
    
    ### New Repository Settings
    
    - `trustedSigning` is a structure defining `Account`, `EndPoint` and
    `CertificateProfile` if you want to use trusted signing. Note that your
    Azure_Credentials secret (Microsoft Entra ID App or Managed identity)
    still needs to provide access to your azure subscription and be assigned
    the `Trusted Signing Certificate Profile Signer` role in the Trusted
    Signing Account.
    - `deployTo<environment>` now has an additional property called
    DependencyInstallMode, which determines how dependencies are deployed if
    GenerateDependencyArtifact is true. Default value is `install` to
    install dependencies if not already installed. Other values are `ignore`
    for ignoring dependencies, `upgrade` for upgrading dependencies if
    possible and `forceUpgrade` for force upgrading dependencies.
    
    ### Support for Azure Trusted Signing
    
    Read https://learn.microsoft.com/en-us/azure/trusted-signing/ for more
    information about Trusted Signing and how to set it up. After setting up
    your trusted signing account and certificate profile, you need to create
    a setting called
    [trustedSigning](https://aka.ms/algosettings#trustedSigning) for AL-Go
    to sign your apps using Azure Trusted Signing.
    
    ### Support for Page Scripting Tests
    
    Page Scripting tests are now supported as part of CI/CD. By specifying
    pageScriptingTests in your project settings file, AL-Go for GitHub will
    automatically run these page scripting tests as part of your CI/CD
    workflow, generating the following build artifacts:
    
    - `PageScriptingTestResults` is a JUnit test results file with all
    results combined.
    - `PageScriptingTestResultDetails` are the detailed test results
    (including videos) when any of the page scripting tests have failures.
    If the page scripting tests succeed - the details are not published.
    
    ### Experimental support for Git submodule
    
    [Git submodule](https://git-scm.com/book/en/v2/Git-Tools-Submodules) is
    now supported as part of CI/CD on your project.
    
    
    [AB#539394](https://dynamicssmb2.visualstudio.com/1fcb79e7-ab07-432a-a3c6-6cf5a88ba4a5/_workitems/edit/539394)
    
    Co-authored-by: bcbuild-github-agent <bcbuild-github-agent@users.noreply.github.com>

--- build/projects/System Application/.AL-Go/localDevEnv.ps1
+++ build/projects/System Application/.AL-Go/localDevEnv.ps1
@@ -46,9 +46,9 @@ Write-Host -ForegroundColor Yellow @'
 
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
-$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go/a46044fa1ffca4b1ff05d2d1844fd7d5922c6984/Actions/Github-Helper.psm1' -folder $tmpFolder
-$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go/a46044fa1ffca4b1ff05d2d1844fd7d5922c6984/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
-DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go/a46044fa1ffca4b1ff05d2d1844fd7d5922c6984/Actions/Packages.json' -folder $tmpFolder | Out-Null
+$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.1/Github-Helper.psm1' -folder $tmpFolder
+$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.1/AL-Go-Helper.ps1' -folder $tmpFolder
+DownloadHelperFile -url 'https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v6.1/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
