b527445758a715ed819b9f14317a34c977cdbb4a
aholstrup <aholstrup@microsoft.com>
2024-12-04 17:11:16 +0100

    Cleanup

--- build/scripts/ImportTestToolkitToBcContainer.ps1
+++ build/scripts/ImportTestToolkitToBcContainer.ps1
@@ -6,16 +6,9 @@ Param(
 # Ordered list of test framework apps to install
 $allApps = (Invoke-ScriptInBCContainer -containerName $containerName -scriptblock { Get-ChildItem -Path "C:\Applications\" -Filter "*.app" -Recurse })
 $testToolkitApps = @(
-    "Permissions Mock", 
-    "Test Runner", 
-    "Any", 
-    "Library Assert", 
-    "Library Variable Storage", 
     "System Application Test Library", 
-    "Business Foundation Test Libraries", 
-    "Performance Toolkit", 
-    "AI Test Toolkit",
-    "Contoso Coffee Demo Dataset" # This is used to generate test data
+    "Business Foundation Test Libraries",
+    "Tests-TestLibraries"
 )
 
 foreach ($app in $testToolkitApps) {
