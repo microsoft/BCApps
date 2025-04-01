ef8e403ee6fca6687441af82498d762736a7ef59
Maria Zhelezova <mazhelez@microsoft.com>
2025-04-01 14:48:55 +0200

    Test

--- build/scripts/CrossBranchPorting.psm1
+++ build/scripts/CrossBranchPorting.psm1
@@ -108,7 +108,7 @@ function New-BCAppsBackport() {
             }
         }
 
-        if(!$PSCmdlet.ShouldProcess()) {
+        if(!$PSCmdlet.ShouldProcess("PR $PullRequestNumber backported")) {
             return # Nothing else to do
         }
 
