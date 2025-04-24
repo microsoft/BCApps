697e3be0b18b73b17885f82ecbe51db64828ec92
aholstrup1 <aholstrup@microsoft.com>
2025-04-24 13:01:01 +0200

    Comments

--- build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/InstallMissingDependencies.ps1
@@ -5,8 +5,8 @@ Param(
 # Reinstall the dependencies in the container
 $customSettings = Get-Content -Path (Join-Path $PSScriptRoot "customSettings.json" -Resolve) | ConvertFrom-Json
 
-
 $isBaseAppInstalled = Get-BcContainerAppInfo -containerName $parameters.containerName -tenantSpecificProperties | Where-Object { ($_.Name -eq "Base Application") -and ($_.IsInstalled -eq $true) }
+# If the base app isn't installed we need to start by installing the app dependencies. Later we will install the test app dependencies.
 if (-not $isBaseAppInstalled) {
     Write-Host "Installing App Dependencies"
     $dependenciesToInstall = $customSettings.ExternalAppDependencies
