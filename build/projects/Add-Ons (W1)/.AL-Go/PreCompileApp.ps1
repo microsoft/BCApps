1d29c75ddd43bc8d86c37d33575191c055762d83
aholstrup1 <aholstrup@microsoft.com>
2025-04-24 13:16:02 +0200

    Refactor

--- build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
@@ -4,17 +4,17 @@ Param(
 )
 
 if ($ENV:BuildMode -eq 'Clean') {
-    Import-Module (Join-Path $PSScriptRoot "../../../scripts/AppExtensionsHelper.psm1" -Resolve)
-
+    
+    # If useProjectDependencies is set to false, we need to remove the app symbols folder so we recompile everything.
     $settings = Get-Content (Join-Path $PSScriptRoot "settings.json" -Resolve) | ConvertFrom-Json
     if ($settings.useProjectDependencies -eq $false) {
-        # Remove everything in the app symbols folder so we recompile everything.
         $symbolsFolder = $compilationParams.Value["appSymbolsFolder"]
         if (Test-Path $symbolsFolder) {
             Remove-Item -Path $symbolsFolder\* -Recurse -Force -Verbose
         }
     }
 
+    Import-Module (Join-Path $PSScriptRoot "../../../scripts/AppExtensionsHelper.psm1" -Resolve)
     $scriptPath = Join-Path $PSScriptRoot "../../../scripts/PreCompileApp.ps1" -Resolve
     . $scriptPath -parameters $compilationParams -appType $appType -recompileDependencies (Get-ExternalDependencies)
 }
