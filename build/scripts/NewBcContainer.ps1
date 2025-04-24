adac0cfc8f720a503ee06dbaabf0c97bc447f9b7
aholstrup1 <aholstrup@microsoft.com>
2025-04-24 13:23:12 +0200

    newbccontainer

--- build/scripts/NewBcContainer.ps1
+++ build/scripts/NewBcContainer.ps1
@@ -13,28 +13,22 @@ if ("$env:GITHUB_RUN_ID" -eq "") {
 
 New-BcContainer @parameters
 
-function PrepareEnvironment() {
-    param(
-        [string] $ContainerName,
-        [boolean] $KeepAppsPublished = $false
-    )
-    $installedApps = Get-BcContainerAppInfo -containerName $ContainerName -tenantSpecificProperties -sort DependenciesLast
+$installedApps = Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast
 
-    # Clean the container for all apps. Apps will be installed by AL-Go
-    foreach($app in $installedApps) {
-        UnInstall-BcContainerApp -containerName $ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
-
-        if ((-not $KeepAppsPublished)) {
-            Write-Host "Unpublishing $($app.Name)"
-            Unpublish-BcContainerApp -containerName $ContainerName -name $app.Name -unInstall -doNotSaveData -doNotSaveSchema -force
-        }
-    }
-
-    foreach ($app in (Get-BcContainerAppInfo -containerName $ContainerName -tenantSpecificProperties -sort DependenciesLast)) {
-        Write-Host "App: $($app.Name) ($($app.Version)) - Scope: $($app.Scope) - IsInstalled: $($app.IsInstalled) - IsPublished: $($app.IsPublished)"
+# Clean the container for all apps. Apps will be installed by AL-Go
+foreach($app in $installedApps) {
+    Write-Host "Removing $($app.Name)"
+    UnInstall-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -doNotSaveData -doNotSaveSchema -force
+    
+    if ((-not $KeepAppsPublished)) {
+        Write-Host "Unpublishing $($app.Name)"
+        Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $app.Name -unInstall -doNotSaveData -doNotSaveSchema -force
     }
 }
 
-PrepareEnvironment -ContainerName $parameters.ContainerName -KeepAppsPublished:$KeepAppsPublished
+Write-Host "Current installed apps in container $($parameters.ContainerName)"
+foreach ($app in (Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast)) {
+    Write-Host "App: $($app.Name) ($($app.Version)) - Scope: $($app.Scope) - IsInstalled: $($app.IsInstalled) - IsPublished: $($app.IsPublished)"
+}
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
