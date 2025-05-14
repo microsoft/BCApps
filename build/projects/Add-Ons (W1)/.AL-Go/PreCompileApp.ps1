a43a0c9849a9b8ae1639e91b72f4c5504d7e0495
aholstrup <aholstrup@microsoft.com>
2025-05-14 10:57:47 +0200

    Debug logging

--- build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
@@ -7,13 +7,15 @@ if ($ENV:BuildMode -eq 'Clean') {
 
     # If useProjectDependencies is set to false, we need to remove the app symbols folder so we recompile everything.
     $settings = $env:Settings | ConvertFrom-Json
-
-    # DEBUG: Log all settings
-    Write-Host "Settings: $env:Settings"
-
     if ($settings.useProjectDependencies -eq $false) {
+        Write-Host "useProjectDependencies is set to false, removing the app symbols folder"
         $symbolsFolder = $compilationParams.Value["appSymbolsFolder"]
         if (Test-Path $symbolsFolder) {
+            # Print the content of the symbols folder before removing it
+            Write-Host "Content of the app symbols folder before removal:"
+            Get-ChildItem -Path $symbolsFolder | ForEach-Object { Write-Host $_.FullName }
+
+            Write-Host "Removing the app symbols folder: $symbolsFolder"
             Remove-Item -Path $symbolsFolder\* -Recurse -Force -Verbose
         }
     }
