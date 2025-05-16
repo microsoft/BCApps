2172c9dbf25a75efb82ee88a907a549aa2b4494b
aholstrup <aholstrup@microsoft.com>
2025-05-16 10:37:51 +0200

    do not delete

--- build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
+++ build/projects/Add-Ons (W1)/.AL-Go/PreCompileApp.ps1
@@ -6,7 +6,7 @@ Param(
 if ($ENV:BuildMode -eq 'Clean') {
 
     # If useProjectDependencies is set to false, we need to remove the app symbols folder so we recompile everything.
-    $settings = $env:Settings | ConvertFrom-Json
+    <#$settings = $env:Settings | ConvertFrom-Json
     if ($settings.useProjectDependencies -eq $false) {
         Write-Host "useProjectDependencies is set to false, removing the app symbols folder"
         $symbolsFolder = $compilationParams.Value["appSymbolsFolder"]
@@ -18,7 +18,7 @@ if ($ENV:BuildMode -eq 'Clean') {
             Write-Host "Removing the app symbols folder: $symbolsFolder"
             Remove-Item -Path $symbolsFolder\* -Recurse -Force -Verbose
         }
-    }
+    }#>
 
     Import-Module (Join-Path $PSScriptRoot "../../../scripts/AppExtensionsHelper.psm1" -Resolve)
     $scriptPath = Join-Path $PSScriptRoot "../../../scripts/PreCompileApp.ps1" -Resolve
