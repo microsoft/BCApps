7cfa62fdd37ffd99c39c57cacd2f5502370b712d
Haoran Sun (Business Central) <haoransun@microsoft.com>
2025-07-09 09:21:42 +0200

    try to run tests on Setup data

--- build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
@@ -2,17 +2,5 @@ Param(
     [Hashtable]$parameters
 )
 
-$parameters["testType"] = "UnitTest"
-
-$script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
-. $script -parameters $parameters
-
-# Integration Tests should run with TestData = Contoso Demo Data (Setup Data)
-$parameters["testType"] = "IntegrationTest"
-$parameters["SetupData"] = $true
-
-$script = Join-Path $PSScriptRoot "../../../scripts/ImportTestDataInBcContainer.ps1" -Resolve
-. $script -parameters $parameters
-
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
 . $script -parameters $parameters
