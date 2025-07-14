d8886137571042ac2aab5fb2cb835f742380d3e6
aholstrup1 <aholstrup@microsoft.com>
2025-07-14 12:53:25 +0200

    test with GenerateReportLayout

--- build/projects/Apps (W1)/.AL-Go/CompileAppInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/CompileAppInBcContainer.ps1
@@ -2,5 +2,7 @@ Param(
     [Hashtable] $parameters
 )
 
+$parameters["GenerateReportLayout"] = "Yes"
+
 $scriptPath = Join-Path $PSScriptRoot "../../../scripts/CompileAppInBcContainer.ps1" -Resolve
 . $scriptPath -parameters $parameters
