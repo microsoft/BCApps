e25595ad28ddd2a0e3253abd9720e6426f4c8e49
Haoran Sun (Business Central) <haoransun@microsoft.com>
2025-07-16 13:22:59 +0200

    uptake requireTestIsolation

--- build/projects/System Application Tests/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/System Application Tests/.AL-Go/RunTestsInBcContainer.ps1
@@ -6,3 +6,5 @@ $parameters["testType"] = "UnitTest"
 
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
 . $script -parameters $parameters
+
+. $script -parameters $parameters -DisableTestIsolation
