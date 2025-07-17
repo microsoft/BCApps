b1e84d2d1605a3ae2af6535901f1973d801a0854
aholstrup1 <aholstrup@microsoft.com>
2025-07-17 10:39:33 +0200

    compute app and platform version

--- .github/actions/TestApplicationManifestConsistency/action.ps1
+++ .github/actions/TestApplicationManifestConsistency/action.ps1
@@ -1,32 +1,36 @@
 function Test-ApplicationManifestConsistency
 {
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$ExpectedAppVersion = (GetApplicationVersion),
+        [Parameter(Mandatory = $false)]
+        [string]$ExpectedPlatformVersion = (GetPlatformVersion)
+    )
     $errors = @()
 
     $bcAppsManifests = (Get-ChildItem "src" -Recurse -Include app.json).FullName
-    $expectedAppVersion = "27.0.0.0"
-    $expectedPlatVersion = "27.0.0.0"
     foreach($path in $bcAppsManifests)
     {
         $manifest = Get-Content $path | ConvertFrom-Json
-        if ($expectedAppVersion -ne $manifest.version)
+        if ($ExpectedAppVersion -ne $manifest.version)
         {
-            $errors += "ERROR: Wrong application version in manifest $path. Expected: $expectedAppVersion. Actual: $($manifest.version)"
+            $errors += "ERROR: Wrong application version in manifest $path. Expected: $ExpectedAppVersion. Actual: $($manifest.version)"
         }
         foreach($item in $manifest.dependencies)
         {
-            if ($expectedAppVersion -ne $item.version)
+            if ($ExpectedAppVersion -ne $item.version)
             {
-                $errors += "ERROR: Wrong dependency version ($($item.name)) in manifest $path. Expected: $expectedAppVersion. Actual: $($item.version)"
+                $errors += "ERROR: Wrong dependency version ($($item.name)) in manifest $path. Expected: $ExpectedAppVersion. Actual: $($item.version)"
             }
         }
-        if ($manifest.platform -and ($expectedPlatVersion -ne $manifest.platform))
+        if ($manifest.platform -and ($ExpectedPlatformVersion -ne $manifest.platform))
         {
             # If path is under App\BCApps, it means the app in the BCApps repository. In that case, do not fail if platform version do not match, as platform version is uptaken in BCApps at a later stage.
             if($path -like "$env:InetRoot\App\BCApps\*") {
                 Write-Host "Skip checking platform version in $path as it is part of the BCApps repository."
             }
             else {
-                $errors += "ERROR: Wrong platform version in manifest $path. Expected: $expectedPlatVersion. Actual: $($manifest.platform)"
+                $errors += "ERROR: Wrong platform version in manifest $path. Expected: $ExpectedPlatformVersion. Actual: $($manifest.platform)"
             }
         }
         if ($manifest.publisher -ne "Microsoft")
@@ -47,6 +51,54 @@ function Test-ApplicationManifestConsistency
     {
         $errors | Write-Host
         throw "Found $($errors.Count) error(s) in application manifest versions"
+    }
+}
+
+function GetApplicationVersion
+{
+    Import-Module (Join-Path $PSScriptRoot "..\..\..\build\scripts\EnlistmentHelperFunctions.psm1" -Resolve)
+    $repoVersion = Get-ConfigValue -Key "repoVersion" -ConfigType AL-GO
+    return "$repoVersion.0.0"
+}
+
+function GetPlatformVersion
+{
+    try {
+        if (-not (Get-Module -ListAvailable -Name "BCContainerHelper")) {
+            Write-Host "BCContainerHelper module not found. Installing..."
+            Install-Module -Name "BCContainerHelper" -Scope CurrentUser -AllowPrerelease -Force
+        }
+        Import-Module BcContainerHelper
+        Import-Module (Join-Path $PSScriptRoot "..\..\..\build\scripts\EnlistmentHelperFunctions.psm1" -Resolve)
+
+        $artifactUrl = Get-ConfigValue -Key "artifact" -ConfigType AL-GO
+
+        if ($artifactUrl -and $artifactUrl -notlike "http*")
+        {
+            $repoVersion = Get-ConfigValue -Key "repoVersion" -ConfigType AL-GO
+
+            $artifactUrl = Get-BCArtifactUrl -select Latest -type Sandbox -country base -version $repoVersion -accept_insiderEula
+            if (-not $artifactUrl) {
+                Get-BCArtifactUrl -select Latest -type Sandbox -country base -version $repoVersion -accept_insiderEula -storageAccount bcinsider
+            }
+        }
+
+        if (-not $artifactUrl) {
+            throw "No artifact URL found. Cannot determine platform version. Using the same as application version."
+        }
+
+        $artifactPath = Download-Artifacts -ArtifactUrl $artifactUrl
+        $manifest = Get-Content (Join-Path $artifactPath "manifest.json" -Resolve) | ConvertFrom-Json
+
+        # Get the platform version
+        [System.Version] $platformVersion = $manifest.platform
+        Write-Host "Using platform version: $($platformVersion.Major).$($platformVersion.Minor).0.0"
+        return "$($platformVersion.Major).$($platformVersion.Minor).0.0"
+    }
+    catch {
+        Write-Host "Failed to get platform version: $_"
+        Write-Host "Using the same version as application version."
+        return GetApplicationVersion
     }
 }
 
