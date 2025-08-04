e4ca74a38721aaa9c784a6d7f960f99d2ab43ca4
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-08-04 08:33:59 +0200

    Update RunTestsInBcContainer.ps1

--- build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
@@ -2,5 +2,9 @@ Param(
     [Hashtable]$parameters
 )
 
+try {
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
 . $script -parameters $parameters
+} catch {
+    Write-Host "-Failed-"
+}
