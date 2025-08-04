469b116954b0cbbe389dd2cb78e6a6b7ccd88126
Haoran Sun (Business Central) <haoransun@microsoft.com>
2025-08-04 16:47:16 +0200

    run tests in three batches

--- build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
@@ -2,5 +2,16 @@ Param(
     [Hashtable]$parameters
 )
 
+$parameters["returnTrueIfAllPassed"] = $true
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
-. $script -parameters $parameters
+
+$parameters["testType"] = "UnitTest"
+[bool] $AllUnitTestsPassed = (. $script -parameters $parameters)
+
+$parameters["testType"] = "IntegrationTest"
+[bool] $AllIntegrationTestsPassed = (. $script -parameters $parameters)
+
+$parameters["testType"] = "Uncategorized"
+[bool] $AllUncategorizedTestsPassed = (. $script -parameters $parameters)
+
+return $AllUnitTestsPassed -and $AllIntegrationTestsPassed -and $AllUncategorizedTestsPassed
