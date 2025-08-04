1d0e0a27baaf0509223999725ad0dae40a085816
Haoran Sun (Business Central) <haoransun@microsoft.com>
2025-08-04 16:50:05 +0200

    Revert "run tests in three batches"
    This reverts commit 469b116954b0cbbe389dd2cb78e6a6b7ccd88126.

--- build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
@@ -2,16 +2,5 @@ Param(
     [Hashtable]$parameters
 )
 
-$parameters["returnTrueIfAllPassed"] = $true
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
-
-$parameters["testType"] = "UnitTest"
-[bool] $AllUnitTestsPassed = (. $script -parameters $parameters)
-
-$parameters["testType"] = "IntegrationTest"
-[bool] $AllIntegrationTestsPassed = (. $script -parameters $parameters)
-
-$parameters["testType"] = "Uncategorized"
-[bool] $AllUncategorizedTestsPassed = (. $script -parameters $parameters)
-
-return $AllUnitTestsPassed -and $AllIntegrationTestsPassed -and $AllUncategorizedTestsPassed
+. $script -parameters $parameters
