9cf6bd828aca924734e0c209d383578f5cbdbac5
aholstrup1 <aholstrup@microsoft.com>
2025-10-22 19:16:36 +0200

    Update scripts

--- build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
@@ -4,17 +4,12 @@ Param(
 
 
 if ($null -ne $env:settings) {
-    Write-Host "AL-Go settings found in environment variable"
     $alGoSettings = $env:settings | ConvertFrom-Json
-    if ($alGoSettings.PSObject.Properties.Name -contains "doNotImportTestData") {
-        if ($alGoSettings.doNotImportTestData -eq $true) {
-            Write-Host "Using test type UnitTest as doNotImportTestData is set to true in AL-Go settings"
-            $parameters["testType"] = "UnitTest"
-        } else {
-            Write-Host "doNotImportTestData is set to false in AL-Go settings. Using default test type."
-        }
+    if (($alGoSettings.PSObject.Properties.Name -contains "doNotImportTestData") -and ($alGoSettings.doNotImportTestData -eq $true)) {
+        Write-Host "Using test type UnitTest as doNotImportTestData is set to true in AL-Go settings"
+        $parameters["testType"] = "UnitTest"
     } else {
-        Write-Host "doNotImportTestData not found in AL-Go settings. Using default test type."
+        Write-Host "doNotImportTestData not found in AL-Go settings or is set to false. Using default test type."
     }
 } else {
     Write-Host "No AL-Go settings found in environment variable. Using default test type."
