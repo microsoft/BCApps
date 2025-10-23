a86f0180377e5197cbee95281bac2680568861a9
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2025-10-23 13:03:45 +0200

    Update action.ps1

--- .github/actions/TestObjectIdsAndManifests/action.ps1
+++ .github/actions/TestObjectIdsAndManifests/action.ps1
@@ -11,5 +11,5 @@ Test-ApplicationIds -SourceCodePaths $sourceCodeFolder
 
 # Test that all manifests are valid
 $currentMajorMinor = Get-ConfigValue -Key "repoVersion" -ConfigType AL-Go
-$expectedPlatformVersion = "$($currentMajorMinor).0.0" # This can be hardcoded to a specific platform version if needed during version updates 
+$expectedPlatformVersion = "$($currentMajorMinor).0.0" # This can be hardcoded to a specific platform version if needed during version updates
 Test-ApplicationManifests -Path $sourceCodeFolder `-ExpectedAppVersion "$($currentMajorMinor).0.0" -ExpectedPlatformVersion $expectedPlatformVersion
