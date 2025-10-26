61a229fccdcf72ded4dd0f18c7596a3ba58dbe9f
aholstrup1 <aholstrup@microsoft.com>
2025-10-26 20:40:40 +0100

    only run for unit tests

--- build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/RunTestsInBcContainer.ps1
@@ -10,6 +10,11 @@ $parameters["returnTrueIfAllPassed"] = $true
 $script = Join-Path $PSScriptRoot "../../../scripts/RunTestsInBcContainer.ps1" -Resolve
 $AllTestsPassed = (. $script -parameters $parameters -TestType $testType)
 
+# TODO: For now only run disabled test isolation for unit tests
+if ($testType -ne "UnitTest") {
+    return $AllTestsPassed
+}
+
 # Run test codeunits with RequiredTestIsolation set to Disabled
 $AllTestsPassedIsolation = (. $script -parameters $parameters -DisableTestIsolation -TestType $testType)
 
