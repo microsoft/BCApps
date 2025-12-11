e6b35ddc515650c7aaba7cc2974fbe29dcc3d4ba
aholstrup1 <aholstrup@microsoft.com>
2025-12-11 09:03:28 +0100

    Evaluation

--- build/scripts/ImportTestDataInBcContainer.ps1
+++ build/scripts/ImportTestDataInBcContainer.ps1
@@ -79,7 +79,7 @@ foreach ($company in $existingCompanies) {
 
 
 Write-Host "Creating new test company in container $($parameters.ContainerName)"
-New-CompanyInBcContainer -containerName $parameters.ContainerName -companyName "CRONUS TestCompany" -evaluationCompany
+New-CompanyInBcContainer -containerName $parameters.ContainerName -companyName "CRONUS TestCompany"
 
 
 # Reinstall all the uninstalled apps in the container
