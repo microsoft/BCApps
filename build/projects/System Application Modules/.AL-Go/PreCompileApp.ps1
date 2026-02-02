0815395691250784c5216962b7b41eec4a441055
aholstrup1 <aholstrup@microsoft.com>
2026-02-02 14:24:28 +0100

    Add precompile override

--- build/projects/System Application Modules/.AL-Go/PreCompileApp.ps1
+++ build/projects/System Application Modules/.AL-Go/PreCompileApp.ps1
@@ -8,5 +8,19 @@ if($appType -eq 'app')
     Write-Host "compilationParams: $compilationParams"
 
     $scriptPath = Join-Path $PSScriptRoot "../../../scripts/VerifyExecutePermissions.ps1" -Resolve
-    . $scriptPath -AppFolder $compilationParams.Value["appProjectFolder"]
+    $workspaceFilePath = $compilationParams.Value["WorkspaceFile"]
+    Write-Host "Reading workspace file from: $workspaceFilePath"
+    $workspaceFile = (Get-Item -Path $workspaceFilePath)
+    $workspace = Get-Content -Path $workspaceFile.FullName -Raw | ConvertFrom-Json
+    Push-Location $workspaceFile.DirectoryName
+    try {
+        # iterate through all projects in the workspace file
+        foreach ($project in $workspace.projects) {
+            $appFolder = $project.path
+            Write-Host "Verifying execute permissions for app folder: $appFolder"
+            . $scriptPath -AppFolder $appFolder
+        }
+    } finally {
+        Pop-Location
+    }
 }
