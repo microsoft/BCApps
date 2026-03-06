42a50668fb8b87eee2d715ea6c86415d370f42e9
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2026-03-06 12:20:02 +0100

    Change AppsToUnpublish parameter in NewBcContainer.ps1
    Updated AppsToUnpublish parameter to include 'AI Test Toolkit' for temporary fix.

--- build/projects/Apps (W1)/.AL-Go/NewBcContainer.ps1
+++ build/projects/Apps (W1)/.AL-Go/NewBcContainer.ps1
@@ -4,4 +4,4 @@ Param(
 Import-Module (Join-Path $PSScriptRoot "../../../scripts/EnlistmentHelperFunctions.psm1" -Resolve)
 
 $script = Join-Path $PSScriptRoot "../../../scripts/NewBcContainer.ps1" -Resolve
-. $script -parameters $parameters -AppsToUnpublish @("None")
+. $script -parameters $parameters -AppsToUnpublish @("AI Test Toolkit") # temp fix to allow renaming 'AI Test Toolkit'
